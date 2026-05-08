9ab9e46421b52a035c02f758b69aa59b7f77072a
Ramon Silva <ramonerik29a@gmail,com>
2026-05-08 16:23:13 -0300

    vue initial commit

---
+++ src/router.d.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+declare module "vue-router" {
+  import {
+    createRouter,
+    createWebHistory,
+    RouteRecordRaw,
+    Router,
+    RouteLocationNormalized,
+    NavigationGuardNext,
+  } from "vue-router/dist/vue-router";
+
+  export {
+    createRouter,
+    createWebHistory,
+    RouteRecordRaw,
+    Router,
+    RouteLocationNormalized,
+    NavigationGuardNext,
+  };
+}
