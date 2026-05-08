afb6feff606e56a3f5cad5dc621c658c13d17be6
Ramon Silva <ramonerik29a@gmail,com>
2026-05-08 17:32:08 -0300

    project name

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
 
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "Fake Store Consumption",
+    },
+  },
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
